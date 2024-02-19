608bbd12ceb527f8df96384a903a45ff32a489ad
Amaara96 <amara.alex96@gmail.com>
2024-02-19 15:08:32 +0100

    My grade

---
+++ check_grade.js
@@ -0,0 +1,24 @@
+let number = prompt("Whats Your Grade")
+
+if (number >= 70 && number <= 100)
+    {
+        console.log("the grade is A");
+    }
+
+else if (number >= 60 && number <= 69)
+    {
+        console.log("The grade is B");
+    }
+else if (number >= 50 && number <= 59)
+    {
+        console.log("The grade is C");
+    
+    }
+
+else if (number <= 49 && number >= 40)
+    {
+        console.log("The grade is D");
+    }
+else {
+    console.log("The grade is F");
+}
---
+++ index.js
@@ -0,0 +1,5 @@
+let str = "Hello my name is Chizzy";
+
+let part = str.slice(-17, -10);
+
+console.log(part);
